1b49c4a4e8b891139f0f6049b0abcd4550626eb0
jmsrzk14 <jmsrizky@gmail.com>
2024-12-29 23:58:38 +0700

    IoTApps

--- src/utils/audio/constants.ts
+++ src/utils/audio/constants.ts
@@ -1,11 +1,11 @@
 // Collection of sound effects URLs
 export const FIREWORK_SOUNDS = {
   launch: [
-    '/asset/fireworks.mp3',
-    '/asset/jingle.mp3', 
+    './asset/fireworks.mp3',
+    './asset/jingle.mp3', 
   ],
   explosion: [
     
-    '/asset/santa.mp3',
+    './asset/santa.mp3',
   ]
 };
